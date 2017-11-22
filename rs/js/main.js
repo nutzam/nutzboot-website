0d0c6369dab49b8b1b7af343aac87364b45a4f05
pangwu86 <pangwu86@gmail.com>
2017-11-22 17:31:07 +0800

    add:新版网页

---
+++ rs/js/main.js
@@ -0,0 +1,33 @@
+// logo加载
+$(window).on('load', function () {
+    $('.status').fadeOut();
+    $('.preloader').delay(350).fadeOut('slow');
+});
+
+// 跳转
+$(function () {
+    $('.navbar-nav a').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 0
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// 其他初始化
+$(document).ready(function () {
+    wow = new WOW(
+        {
+            animateClass: 'animated',
+            offset: 100,
+            mobile: true
+        }
+    );
+    wow.init();
+
+    $('.counter').counterUp({
+        delay: 100,
+        time: 1200
+    });
+});
